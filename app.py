f54ecc29bda9da46c947affcb303c3af6bd64407
John Florkey <john.florkey@gmail.com>
2021-05-01 19:26:42 -0400

    Added stubbed out files for basic flask project.

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
+from models import connect_db, db
+
+app = Flask(__name__)
+
+# Get DB_URI from environ variable (useful for production/testing) or,
+# if not set there, use development local db.
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    os.environ.get('DATABASE_URL', 'postgres:///personal-library'))
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+toolbar = DebugToolbarExtension(app)
+
+connect_db(app)
+db.create_all()
